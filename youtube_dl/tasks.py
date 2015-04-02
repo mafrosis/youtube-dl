7e8da20fe1db910af54b1d905826280fa499a92f
mafro <github@mafro.net>
2015-04-03 08:28:50 +1100

    more logic updates

--- youtube_dl/tasks.py
+++ youtube_dl/tasks.py
@@ -10,6 +10,7 @@ import traceback
 
 from . import celery_app
 
+DEBUG = False
 
 
 @celery_app.task
@@ -19,7 +20,12 @@ def import_from_youtube(url, log_to_stdout=False):
     output, stderr = None, None
 
     try:
-        # TODO elementary handling of a double click..
+        # TODO use temp directory for each download
+        # - named with hash of url (which acts as the lock)
+        # - provides clean up after
+        # TODO switch to python logging, with levels
+        # - inherit celery's loglevel ??
+
         lock = md5.new(url).hexdigest()
         if os.path.exists('/tmp/{}.lock'.format(lock)):
             print '[WARNING] This url is already being downloaded: {}'.format(url)
@@ -34,7 +40,7 @@ def import_from_youtube(url, log_to_stdout=False):
         # download stream
         print '[INFO] Downloading {}'.format(url)
         proc = subprocess.Popen(
-            'youtube-dl --write-info-json --no-progress --socket-timeout 10 {}'.format(url),
+            'youtube-dl --write-info-json --no-progress --socket-timeout 10 {} 2>&1'.format(url),
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             shell=True,
@@ -43,12 +49,18 @@ def import_from_youtube(url, log_to_stdout=False):
         output, stderr = proc.communicate()
 
         if proc.returncode != 0:
-            raise DownloadError(stderr)
+            raise DownloadError(output)
 
         for line in output.splitlines():
+            if DEBUG:
+                print '[DEBUG] {}'.format(line)
+
             if line.startswith('[download] Destination: '):
-                filename = line[24:39]
+                filename = line[24:]    # get the filename which is remainder of the line
                 break
+            #if line.startswith('[download] Destination: '):
+            #    filename = line[24:39]
+            #    break
             elif line.endswith('has already been downloaded'):
                 filename = line[11:-28]
                 print '[INFO] Already downloaded /tmp/{}'.format(filename)
@@ -60,36 +72,70 @@ def import_from_youtube(url, log_to_stdout=False):
 
         print '[INFO] Processing {}'.format(filename)
 
+        infok = False
+
         # parse the info file for tagging
-        with open('/tmp/{}.info.json'.format(os.path.splitext(filename)[0]), 'r') as f:
-            info = json.loads(f.read())
+        infofile = '/tmp/{}.info.json'.format(os.path.splitext(filename)[0])
+
+        if os.path.exists(infofile) is True:
+            with open(infofile, 'r') as f:
+                info = json.loads(f.read())
+                infok = True
+        else:
+            print '{} not found, no tagging today :('.format(infofile)
 
         # use the movie's meta title as the mp3 filename
-        outputname = '{}.mp3'.format(re.sub('[^a-zA-Z0-9]', '_', info['stitle']))
+        if infok:
+            outputname = '{}.mp3'.format(re.sub('[^a-zA-Z0-9]', '_', info['stitle']))
+        else:
+            # default filename to youtube title
+            outputname = filename
+            if os.path.splitext(outputname)[1] != 'mp3':
+                outputname = '{}.mp3'.format(re.sub('[^a-zA-Z0-9]', '_', outputname))
 
         if os.path.splitext(filename)[1] != 'mp3':
             # convert to MP3
             print '[INFO] Converting to mp3'
-            subprocess.check_call(
-                "/usr/bin/ffmpeg -i '/tmp/{}' -loglevel panic -y -vn -acodec libmp3lame -ar 44100 -ac 2 '/tmp/{}'".format(
-                    filename, outputname
-                ), shell=True
+
+            ffmpeg_cmd = (
+                "/usr/bin/ffmpeg -i '/tmp/{}' "
+                "-loglevel panic -y -vn "
+                "-acodec libmp3lame -ar 44100 -ac 2 "
+                "'/tmp/{}' 2>&1"
+            ).format(
+                filename, outputname
             )
+            if DEBUG:
+                print '[DEBUG] {}'.format(ffmpeg_cmd)
+
+            # convert it
+            ffmpeg_output = subprocess.check_output(ffmpeg_cmd, shell=True)
+
+            if DEBUG:
+                print '[DEBUG] {}'.format(ffmpeg_output)
         else:
             # rename the file
             os.rename(filename, outputname)
 
         # elementary tagging
-        print '[INFO] Tagging {}'.format(outputname)
-        subprocess.check_call(
-            (
+        if infok:
+            print '[INFO] Tagging {}'.format(outputname)
+
+            eyed3_cmd = (
                 "/usr/bin/eyeD3 --no-color -a '{}' -t '{}' -A 'Youtube-DL' "
                 "--set-user-text-frame 'Ripping Tool:youtube-dl' "
-                "--set-url-frame 'WOAF:{}' /tmp/{}"
+                "--set-url-frame 'WOAF:{}' '/tmp/{}' 2>&1"
             ).format(
                 info['title'], info['title'], url, outputname
-            ), shell=True
-        )
+            )
+            if DEBUG:
+                print '[DEBUG] {}'.format(eyed3_cmd)
+
+            # tag it
+            eyed3_output = subprocess.check_output(eyed3_cmd, shell=True)
+
+            if DEBUG:
+                print '[DEBUG] {}'.format(eyed3_output)
 
         # move the new file into mpd lib
         subprocess.call(
